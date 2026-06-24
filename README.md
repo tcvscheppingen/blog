0c7eac1a0033783379806bf701b3b122bfedfd1d
Thymen van Scheppingen <thymen.van.scheppingen@intus.nl>
2026-06-24 16:43:06 +0200

    updated some pages

--- README.md
+++ README.md
@@ -53,6 +53,7 @@ All commands are run from the root of the project, from a terminal:
 | `pnpm preview`         | Preview your build locally, before deploying     |
 | `pnpm astro ...`       | Run CLI commands like `astro add`, `astro check` |
 | `pnpm astro -- --help` | Get help using the Astro CLI                     |
+| `node ./dist/server/entry.mjs` | Run build with node                      | 
 
 ## 👀 Want to learn more?
 
--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+
 	fonts: [
 		{
 			provider: fontProviders.local(),
@@ -32,4 +35,8 @@ export default defineConfig({
 			},
 		},
 	],
+	output: 'server',
+	adapter: node({
+		mode: 'standalone',
+	}),
 });
