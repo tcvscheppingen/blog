09d82f16c6b0a9b92b742318da51a1ca24c2373c
tcvscheppingen <t.c.scheppingen@protonmail.com>
2026-09-24 11:24:23 +0200

    Added preview images

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://thymenvscheppingen.statichost.page",
   integrations: [mdx(), sitemap()],
   prefetch: {
     defaultStrategy: 'load'
