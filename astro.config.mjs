5acdd50439bf8888219ed95068d33da0b434caf2
tcvscheppingen <t.c.scheppingen@protonmail.com>
2026-09-25 13:38:54 +0200

    Added Prism syntax hightlighting and youtube embed

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,7 +16,8 @@ export default defineConfig({
   markdown: {
     processor: unified({
       remarkPlugins: [remarkReadingTime]
-    })
+    }),
+    syntaxHighlight: "prism",
   },
   security: {
     csp: true,
